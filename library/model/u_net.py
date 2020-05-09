953d0378b1f797b5c84887206c103836a9939d77
futartup <futartup@gmail.com>
2020-05-09 15:39:18 +0530

    Update u_net.py

--- library/model/u_net.py
+++ library/model/u_net.py
@@ -2,121 +2,115 @@ import numpy
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchsummary import summary
 
-class UNet(nn.Module):
-    def __init__(self, in_planes, planes, stride=1):
-        super(DepthPrediction, self).__init__()
-        self.conv64 = nn.Sequential(
-            nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.Relu(),
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
 
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.Relu(),
-        )
-        self.mp1 = nn.MaxPool2d(2)
-
-        self.conv128 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(128),
-            nn.Relu(),
-
-            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(128),
-            nn.Relu(),
-        )
-        self.mp2 = nn.MaxPool2d(2)
-
-        self.conv256 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.Relu(),
-
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.Relu(),
-        )
-        self.mp3 = nn.MaxPool2d(2)
-
-        self.conv512 = nn.Sequential(
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.Relu(),
-
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.Relu(),
-        )
-        self.mp4 = nn.MaxPool2d(2)
-
-        self.conv1024 = nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1, bias=False),
-
-        self.upsample1 = nn.Sequential(
-            nn.ConvTranspose2d(1024, 1024, kernel_size=2, stride=2),
-            nn.BatchNorm2d(1024),
-            nn.Relu(),
-
-            nn.Conv2d(1024, 512, kernel_size=3, stride=2),
-            nn.BatchNorm2d(1024),
-            nn.Relu(),
-        )
-
-        self.upsample2 = nn.Sequential(
-            nn.ConvTranspose2d(512, 512, kernel_size=2, stride=2, bias=False),
-            nn.BatchNorm2d(512),
-            nn.Relu(),
-
-            nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.Relu(),
-        )
-
-        self.upsample3 = nn.Sequential(
-            nn.ConvTranspose2d(256, 256, kernel_size=2, stride=2, bias=False),
-            nn.BatchNorm2d(256),
-            nn.Relu(),
-
-            nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(128),
-            nn.Relu(),
-        )
-
-        self.upsample4 = nn.Sequential(
-            nn.ConvTranspose2d(128, 128, kernel_size=2, stride=2, bias=False),
-            nn.BatchNorm2d(128),
-            nn.Relu(),
-
-            nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.Relu(),
-
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.Relu(),
-
-            nn.Conv2d(64, 2, kernel_size=1, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(2),
-            nn.Relu(),
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
         )
 
     def forward(self, x):
-        out = self.conv64(x)
-        out_conv64 = out 
-        out = self.mp1(out)
-        out = self.conv128(out)
-        out_conv128 = out
-        out = self.mp2(out)
-        out = self.conv256(out)
-        out_conv256 = out 
-        out = self.mp3(out)
-        out = self.conv512(out)
-        out_conv512 = out 
-        out = self.mp4(out)
-        out = self.conv1024(out)
-        out = torch.cat([self.upsample1(out), out_conv512], dim=1)
-        out = torch.cat([self.upsample2(out), out_conv256, dim=1)
-        out = torch.cat([self.upsample3(out), out_conv128, dim=1)
-        out = torch.cat([self.upsample4(out), out_conv64, dim=1)
+        return self.double_conv(x)
 
-        return out 
+
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels , in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
+
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=True):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
+
+net = UNet(1, 2, False)
+use_cuda = torch.cuda.is_available()
+print(use_cuda)
+if use_cuda:
+    torch.cuda.manual_seed(1)
+device = torch.device("cuda" if use_cuda else "cpu")
+model = net.to(device)
+summary(model, input_size=(1, 224, 224))
