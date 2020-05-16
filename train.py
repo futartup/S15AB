c1e444000de28c81fd22dd4cdb62bd1a87f354b1
futartup <futartup@gmail.com>
2020-05-16 18:00:14 +0530

    Update train.py

--- train.py
+++ train.py
@@ -126,7 +126,7 @@ class Main:
             checkpoint = torch.load(self.load_model)
             self.model = model_obj.return_model()
             self.conf['epochs'] = checkpoint.get('epoch', self.conf['epochs'])
-            self.model = self.model.load_state_dict(checkpoint['state_dict'])
+            self.model = self.model.load_state_dict(checkpoint.get('state_dict', self.model))
             self.optimizer = self.model.load_state_dict(checkpoint.get('optimizer', self.get_optimizer()))
             self.device = model_obj.get_device()
 
