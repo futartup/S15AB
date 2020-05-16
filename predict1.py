ce458f5060a1e7bac7d50adeb0be7d8ebbd8ed0e
futartup <futartup@gmail.com>
2020-05-16 16:41:51 +0530

    Update predict1.py

--- predict1.py
+++ predict1.py
@@ -13,19 +13,32 @@ from library.model.u_net import UNet
 from library.loader.data_loader import DepthDataSet
 
 def plot_img_and_mask(img, mask, filename):
-    classes = mask.shape[2] if len(mask.shape) > 2 else 1
-    fig, ax = plt.subplots(1, classes + 1)
+    
+    fig, ax = plt.subplots(1, 3)
+  
+    # ax[0].imshow(img)
+    # ax[1].imshow(mask[0 , :, :])
+    #ax[2].imshow(mask[1 , :, :])
+    #plt.show()
+    #classes = mask.shape[2] if len(mask.shape) > 2 else 1
+    #fig, ax = plt.subplots(1, 3)
+    # plt.figure(1)
+    # plt.subplot(211)
+    # plt.imshow(img)
+
+    # plt.subplot(212)
+    # plt.imshow(mask[0 , :, :])
+
+    # plt.subplot(222)
+    # plt.imshow(mask[1 , :, :])
+
     ax[0].set_title('Input image')
     ax[0].imshow(img)
-    if classes > 1:
-        for i in range(classes):
-            ax[i+1].set_title(f'Output mask (class {i+1})')
-            ax[i+1].imshow(mask[:, :, i])
-    else:
-        ax[1].set_title(f'Output mask')
-        ax[1].imshow(mask)
-    plt.xticks([]), plt.yticks([])
-    #plt.savefig('/content/drive/My Drive/Colab Notebooks/S15AB/data/output/{}'.format(filename))
+    ax[1].set_title(f'Output firt mask')
+    ax[1].imshow(mask[0 , :, :])
+    ax[2].set_title(f'Output second mask')
+    ax[2].imshow(mask[1 , :, :])
+    # plt.xticks([]), plt.yticks([])
     plt.savefig('/content/drive/My Drive/Colab Notebooks/S15AB/{}'.format(filename))
 
 def predict_img(net,
