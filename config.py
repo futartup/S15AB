770cc168123ffc266bd943fc40b18551aa11a911
futartup <futartup@gmail.com>
2020-05-20 18:56:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
 {
     "seed": 1,
     "data": "custom",
-    "mean": [0.43072554273179153, 0.4403776258001769, 0.4335964820201932] ,
-    "std": [0.28761761089190296, 0.2675504910801714, 0.2683254497043513],
     "num_workers": 2,
     "shuffle": true,
     "epochs": 50,
     "multiclass": true,
-    "batch_size": 30,
+    "batch_size": 10,
     "num_workers": 2,
     "lr_step_size": 25,
     "lr_gamma": 0.001,
@@ -17,8 +15,9 @@
         "range_test": {"end_lr":100, "step_mode": "exp"}
     },
     "optimizer": {
-        "type": "Adam",
-        "weight_decay": 1e-8
+        "type": "RMSprop",
+        "weight_decay": 1e-8,
+        "momentum": 0.9
     },
     "scheduler": {
         "type": "ReduceLROnPlateau",
@@ -35,13 +34,24 @@
                 "num_holes":1,
                 "max_h_size": 80, 
                 "max_w_size": 80
+            },
+            {
+                "name": "Normalize",
+                "mean": [0.42969442521673096, 0.4398552525513781, 0.4328128029756167] ,
+                "std":[0.28715867313017185, 0.26689101668717086, 0.2673114311850263],
+                "always_apply": true
             }
         ]
         },
         "test": {
             "which": "albumentation",
             "what": [
-                
+            {
+                "name": "Normalize",
+                "mean": [0.43169562107826576, 0.44226087717112156, 0.43416351285063187] ,
+                "std":[0.28766322395057514, 0.2674202933537084, 0.2684091646207769],
+                "always_apply": true
+            }
             ]
         }
     },
