cd91fc405e00f431ed66e37de96e1c93fbfc84e7
futartup <futartup@gmail.com>
2020-05-26 22:16:02 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import json
+import time
+import copy
 import uuid
 import torch
 import logging
@@ -16,6 +18,7 @@ from datetime import datetime
 import torch.nn.functional as F
 import torchvision.transforms as T
 from matplotlib import pyplot as plt
+import torch.nn.utils.prune as prune
 from library.lr_finder import LRFinder
 from library.custom_loss import DiceCoeff
 from library.model.get_model import GetModel
@@ -42,9 +45,8 @@ class Main:
         self.data_dir = data_dir
         self.get_loaders() # get the test and train loaders
         self.load_model = load_model
-        assert self.conf['loss'] in globals(), "The loss function name doesn't match with names available"
         self.criterion = globals()[self.conf['loss']['mask'][0]]()
-        self.criterion_depth = globals()[self.conf['loss']['depth'][1]]()
+        self.criterion_depth = globals()[self.conf['loss']['depth'][0]]()
         self.get_model()
         
         if not hasattr(self, 'optimizer'):
@@ -59,18 +61,7 @@ class Main:
         #self.visualize_tranformed_data() # visualize the transformed data
         if self.conf['lr_finder_use']:
             self.lr_finder() # Find the best lr 
-        train_acc = []
-        test_acc = []
-        train_loss = []
-        tests_loss = []
-
-        # Globals step
-        global_step_train = 0
-        global_step_test = 0
-
-        train_loss_decrease = 0
-        test_loss_decrease = 0
-
+        
         logging.info(f'''   Starting training:
                             Epochs:          {self.conf['epochs']}
                             Batch size:      {self.conf['batch_size']}
@@ -81,29 +72,196 @@ class Main:
                     )
 
         
+
+        val_acc_history = []
+        global_step = 0
+        best_model_wts = copy.deepcopy(model.state_dict())
+        best_acc = 0.0
         for e in range(1, self.conf['epochs']):
+            since = time.time()
             print("================================")
             print("Epoch number : {}".format(e))
-            self.train(e, train_acc, train_loss, train_loss_decrease, global_step_train)
-            
-            val_loss = self.test(test_acc, tests_loss, test_loss_decrease, global_step_test)
-            self.scheduler.step(val_loss)
-            print("================================")
+            if 'prunning' in self.conf['model_optimization'] and self.conf['model_optimization']['prunning']:
+                self.conf['model_optimization'].pop('')
+                parameters_to_prune = (
+                                        (self.model.down1, 'weight'),
+                                        (self.model.down2, 'weight'),
+                                        (self.model.down3, 'weight'),
+                                        (self.model.down4, 'weight'),
+                                        (self.model.up1, 'weight'),
+                                        (self.model.up2, 'weight'),
+                                        (self.model.up3, 'weight'),
+                                        (self.model.up4, 'weight'),
+                                        (self.model.outc, 'weight'),
+                                      )
+                prune.global_unstructured(
+                                            parameters_to_prune,
+                                            pruning_method=prune.L1Unstructured,
+                                            amount=0.2,
+                                         )
+                self.sparsity()
+            # self.train(e, train_acc, train_loss, train_loss_decrease, global_step_train)
+            # val_loss = self.test(test_acc, tests_loss, test_loss_decrease, global_step_test)
+            # self.scheduler.step(val_loss)
+            for phase in ['train', 'test']:
+                if phase == 'train':
+                    self.model.train()
+                else:
+                    self.model.eval()
+                
+                total = 0
+                running_loss = 0
+                running_corrects = 0
 
-        self.plot_graphs(train_loss, tests_loss, train_acc, test_acc)
-        
-        # Save the current model
-        current_directory = os.getcwd() 
-        checkpoint = {
-                        'epoch': self.conf['epochs'] + 1,
+                for batch in self.dataloaders[phase]:
+                    bg = batch['bg'].to(device=self.device, dtype=torch.float) # bg images
+                    fg_bg = batch['image'].to(device=self.device, dtype=torch.float) # fg_bg images
+                    mask = batch['mask'].to(device=self.device, dtype=torch.float) # the mask images
+                    depth = batch['depth'].to(device=self.device, dtype=torch.float) # the depth images produced from densedepth
+
+                    # make the parameter gradients zero
+                    self.optimizer.zero_grad()
+
+                    # forward
+                    # track the history only when we are in training phase
+                    with torch.set_grad_enabled(phase == 'train'):
+                        # Get the model inputs, which are fg_bg and bg.
+                        # 
+                        # Send the images to model, and get the output
+                        images = torch.cat([fg_bg, bg], dim=1)
+                        mask_pred, depth_pred = self.model(images)
+                        
+                        mask_loss = self.criterion(mask_pred, mask) # the mask loss
+                        depth_loss = self.criterion_depth(depth_pred, depth) # the depth loss
+                        loss = mask_loss + 0.4 * depth_loss
+                            
+                        if phase == 'train':
+                            loss.backward()
+                            self.optimizer.step()
+                            self.scheduler.step()
+
+                    running_loss += loss.item()
+                    _, predicted_mask = torch.max(mask_pred.data, 1)
+                    _, predicted_depth = torch.max(depth_pred.data, 1)
+                    
+                    total += mask.nelement() + depth.nelement()
+                    running_corrects += predicted_mask.eq(mask.data).sum().item() + predicted_depth.eq(depth.data).sum().item()
+            
+            epoch_loss = running_loss / len(self.dataloaders[phase])
+            epoch_acc = running_corrects.double() / len(self.dataloaders[phase])
+            
+            # Load to tesnsorboard
+            self.writer.add_scalar('Loss/{}'.format(phase), epoch_loss, global_step)
+            self.writer.add_scalar('Loss/{}'.format(phase), epoch_acc, global_step)
+
+            # Print to console
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # Deep copy the model
+            if phase == 'test' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(self.model.state_dict())
+                current_directory = os.getcwd() 
+                checkpoint = {
                         'state_dict': self.model.state_dict(),
                         'optimizer': self.optimizer.state_dict()
                      }
-        torch.save(checkpoint, '/content/drive/My Drive/Colab Notebooks/class-{0}_epoch_{1}_{2}_{3}.pth'.format(self.conf['model_initializer']['n_classes'], 
+                torch.save(checkpoint, current_directory + '/saved-models/epoch_{1}_{2}_{3}.pth'.format( 
                                                                                                          self.conf['epochs'], 
                                                                                                          datetime.now(), 
-                                                                                         uuid.uuid4()))
+                                                                                                         uuid.uuid4())
+                                                                                                       )
+            time_elapsed = time.time() - since
+            print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+            global_step += 1
+            print("================================")
+
+        print('Best val Acc: {:4f}'.format(best_acc))
+        self.plot_graphs(train_loss, tests_loss, train_acc, test_acc)
+        
         self.writer.close()
+
+    def sparsity(self):
+        print(
+                "Sparsity in self.model.down1.weight: {:.2f}%".format(
+                    100. * float(torch.sum(self.model.down1.weight == 0))
+                    / float(self.model.down1.weight.nelement())
+                )
+        )
+        print(
+            "Sparsity in self.model.down2.weight: {:.2f}%".format(
+                100. * float(torch.sum(self.model.down2.weight == 0))
+                / float(self.model.down2.weight.nelement())
+            )
+        )
+        print(
+            "Sparsity in self.model.down3.weight: {:.2f}%".format(
+                100. * float(torch.sum(self.model.down3.weight == 0))
+                / float(self.model.down3.weight.nelement())
+            )
+        )
+        print(
+            "Sparsity in self.model.down4.weight: {:.2f}%".format(
+                100. * float(torch.sum(self.model.down4.weight == 0))
+                / float(self.model.down4.weight.nelement())
+            )
+        )
+        print(
+            "Sparsity in self.model.up1.weight: {:.2f}%".format(
+                100. * float(torch.sum(self.model.up1.weight == 0))
+                / float(self.model.up1.weight.nelement())
+            )
+        )
+        print(
+            "Sparsity in self.model.up2.weight: {:.2f}%".format(
+                100. * float(torch.sum(self.model.up2.weight == 0))
+                / float(self.model.up2.weight.nelement())
+            )
+        )
+        print(
+            "Sparsity in self.model.up3.weight: {:.2f}%".format(
+                100. * float(torch.sum(self.model.up3.weight == 0))
+                / float(self.model.up3.weight.nelement())
+            )
+        )
+        print(
+            "Sparsity in self.model.up4.weight: {:.2f}%".format(
+                100. * float(torch.sum(self.model.up4.weight == 0))
+                / float(self.model.up4.weight.nelement())
+            )
+        )
+        print(
+            "Sparsity in self.model.outc.weight: {:.2f}%".format(
+                100. * float(torch.sum(self.model.outc.weight == 0))
+                / float(self.model.outc.weight.nelement())
+            )
+        )
+        print(
+            "Global sparsity: {:.2f}%".format(
+                100. * float(
+                    torch.sum(self.model.down1.weight == 0)
+                    + torch.sum(self.model.down2.weight == 0)
+                    + torch.sum(self.model.down3.weight == 0)
+                    + torch.sum(self.model.down4.weight == 0)
+                    + torch.sum(self.model.up1.weight == 0)
+                    + torch.sum(self.model.up2.weight == 0)
+                    + torch.sum(self.model.up3.weight == 0)
+                    + torch.sum(self.model.up4.weight == 0)
+                    + torch.sum(self.model.outc.weight == 0)
+                )
+                / float(
+                    self.model.down1.weight.nelement()
+                    + self.model.down2.weight.nelement()
+                    + self.model.down3.weight.nelement()
+                    + self.model.down4.weight.nelement()
+                    + self.model.up1.weight.nelement()
+                    + self.model.up2.weight.nelement()
+                    + self.model.up3.weight.nelement()
+                    + self.model.up4.weight.nelement()
+                    + self.model.outc.weight.nelement()
+                )
+            )
+        )
         
     def plot_graphs(self, train_loss, tests_loss, train_acc, test_acc):
         plt.figure(figsize=(8,8))
@@ -121,22 +279,6 @@ class Main:
         plt.figure(figsize=(8,8))
         plt.plot(test_acc)
         plt.savefig("/content/drive/My Drive/Colab Notebooks/test_acc.jpg")
-
-    def visualize_tranformed_data(self):
-        images = next(iter(self.train_loader))
-        fg_bg = images['image']
-        #mask = images['mask'][:5]
-        #depth = images['depth'][:5]
-        #final_image = fg_bg + mask + depth
-        grid = torchvision.utils.make_grid(fg_bg)
-        self.writer.add_image('Transformed images', grid)
-        #self.writer.add_graph(self.model, images['image'])
-        # count = 0
-        # for im in images:
-        #   im = T.ToPILImage(mode="RGB")(im)
-        #   im.save('/content/drive/My Drive/Colab Notebooks/S15A-B/transformed_images/{}.jpg'.format(count))
-        #   im.close()
-        #   count += 1
 
     def get_model(self):
         model_obj = GetModel(self.conf)
@@ -160,9 +302,12 @@ class Main:
                               self.data_dir + '/depth',
                               self.data_dir + '/bg',
                               .30)
-        self.train_loader = obj.get_train_loader()
-        self.test_loader = obj.get_test_loader()
-        print("Total length of train and test is : {} and {}".format(len(self.train_loader), len(self.test_loader)))
+        #self.train_loader = obj.get_train_loader()
+        #self.test_loader = obj.get_test_loader()
+        self.dataloaders = {}
+        self.dataloaders['train'] = obj.get_train_loader()
+        self.dataloaders['test'] = obj.get_test_loader()
+        print("Total length of train and test is : {} and {}".format(len(self.dataloaders['train']), len(self.dataloaders['test'])))
 
     def test(self, test_acc, tests_loss, test_loss_decrease, global_step_test):
         self.model.eval()
@@ -318,12 +463,12 @@ if __name__ == '__main__':
         with open(BASE_DIR + '/config-schema.json', 'r') as fp:
             json_schema = json.load(fp)
             error_messages = []
-            v = jsonschema.Draft6Validator(json_schema)
-            errors = sorted(v.iter_errors(conf), key=lambda e: e.path)
-            for error in errors:
-                    error_messages.append({error.message: error.instance})
-            print(error_messages)
-            if bool(error_messages):
-                sys.exit(0)
+            # v = jsonschema.Draft6Validator(json_schema)
+            # errors = sorted(v.iter_errors(conf), key=lambda e: e.path)
+            # for error in errors:
+            #         error_messages.append({error.message: error.instance})
+            # print(error_messages)
+            # if bool(error_messages):
+            #     sys.exit(0)
     
         Main(conf, args.get('data_dir'), args.get('load_model'))
